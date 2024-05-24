e4a35c6887642006ef7cf6f19330fa2d79239eaa
Your Name <viduong@uw.edu>
2024-05-24 10:55:29 -0700

    fixed widget

--- lib/legend/legend.dart
+++ lib/legend/legend.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LegendWidget extends StatelessWidget {
-  const LegendWidget({Key? key}) : super(key: key);
+  const LegendWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
